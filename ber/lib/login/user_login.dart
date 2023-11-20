a20800d7a8a0a66c091d8cee5f04d6c20c126ce5
ArbSoftware <satabhishar@arbsoft.comm>
2023-11-20 17:48:23 +0530

    modified

---
+++ ber/lib/login/user_login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class UserLogin extends StatefulWidget {
+  // const UserLogin({Key? key}) : super(key: key);
+  static String id = 'Login';
+
+  @override
+  State<UserLogin> createState() => _UserLoginState();
+}
+
+class _UserLoginState extends State<UserLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
