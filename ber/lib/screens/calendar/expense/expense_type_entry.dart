fbc57253b23cd975616ce5098801c1e36f1be2ea
ArbSoftware <satabhishar@arbsoft.comm>
2023-12-06 14:11:20 +0530

    modified

---
+++ ber/lib/screens/calendar/expense/expense_type_entry.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class ExpenseTypeEntry extends StatefulWidget {
+  const ExpenseTypeEntry({Key? key}) : super(key: key);
+
+  @override
+  State<ExpenseTypeEntry> createState() => _ExpenseTypeEntryState();
+}
+
+class _ExpenseTypeEntryState extends State<ExpenseTypeEntry> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
