b87726a67e1fa588db3b3307adb487ce9b1ff6dd
ArbSoftware <satabhishar@arbsoft.comm>
2023-12-06 17:57:14 +0530

    modified

--- ber/lib/screens/calendar/expense/expense_type_entry.dart
+++ ber/lib/screens/calendar/expense/expense_type_entry.dart
@@ -56,6 +56,41 @@ class _ExpenseTypeEntryState extends State<ExpenseTypeEntry> {
               )
             ],
           ),
+          body: Container(
+            width: MediaQuery.of(context).size.width,
+            child: SafeArea(
+              child: Column(
+                children: [
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      color: Color.fromRGBO(117, 185, 223, 1.0),
+                      // color: Colors.red,
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            RichText(text: TextSpan(
+                            style: DefaultTextStyle.of(context).style,
+                            children: [
+                              TextSpan(text: 'Expense type:', style: TextStyle(fontSize:15, color: Colors.white, decoration: TextDecoration.none),),
+                              TextSpan(text: ' Travel', style: TextStyle(fontSize: 15, color: Color.fromRGBO(
+                                  138, 109, 76, 1.0), decoration: TextDecoration.none)),
+                            ]
+                            )),
+                            Image.asset('images/downarrow.png', width: 40, height: 40,),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         )
     );
   }
